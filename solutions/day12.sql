de9feec594cb3bda8fc324e328cdc7fb3d02a9e0
Samyukta B <samyuktab22@gmail.com>
2025-11-15 19:27:13 +0530

    Added Day 12 SQL solution and result table

--- solutions/day12.sql
+++ solutions/day12.sql
@@ -4,9 +4,9 @@ event status ('With Event' or 'No Event'), count of weeks,
 average patient satisfaction, and average staff morale. 
 Order by average patient satisfaction descending. */
 
-select count(week), avg(patient_satisfaction), avg(staff_morale),
+select count(week), round(avg(patient_satisfaction),2), round(avg(staff_morale),2),
 CASE
-	WHEN (event != "none") THEN 'With Event'
+	WHEN (event != "none" and event IS NOT NULL) THEN 'With Event'
 	ELSE 'No Event'
 END as event_status
 from services_weekly
