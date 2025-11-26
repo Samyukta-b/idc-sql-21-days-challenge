93021925269b6af0092a85038848c3e7629b9af5
Samyukta B <samyuktab22@gmail.com>
2025-11-26 18:54:44 +0530

    Add Day 19 SQL solution and result table

---
+++ solutions/day19.sql
@@ -0,0 +1,16 @@
+/*Question: For each service, rank the weeks by patient satisfaction 
+score (highest first). Show service, week, patient_satisfaction, patients_admitted, 
+and the rank. Include only the top 3 weeks per service.*/
+
+SELECT * FROM (
+    SELECT
+        service,
+        week,
+        patient_satisfaction,
+        patients_admitted,
+        ROW_NUMBER() OVER (PARTITION BY service ORDER BY patient_satisfaction DESC) AS sat_rank
+    FROM services_weekly
+) as main_table
+WHERE sat_rank <= 3
+
+-- Result table in ./`result tables`/day19.csv
